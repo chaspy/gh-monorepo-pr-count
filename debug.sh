122936853606452312cd07af3ad44b73ae3f465e
kondo takeshi <chaspy@users.noreply.github.com>
2024-01-07 13:38:25 +0900

    fix debug script

--- debug.sh
+++ debug.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 since="2023-10-01"
+GH_BASE="develop"
+GH_LABEL="api"
+
 gh pr list --base "${GH_BASE}" --repo "${GH_REPO}" --label ${GH_LABEL} --search "merged:>=${since} -label:dependencies" --limit 100
